d5d7c579e49564b4aa6deb871da5b22fde6d0ca9
nguyenvantan <nguyenvantan@chotot.vn>
2016-03-24 17:49:27 +0700

    update test

--- src/components/HeaderPanel/HeaderPanel.js
+++ src/components/HeaderPanel/HeaderPanel.js
@@ -14,8 +14,6 @@ export default class HeaderPanel extends Component {
 
   render() {
     //const styles = require('./HeaderPanel.less'); className={styles.headerPanel}
-    //const style= require("!raw!sass!./HeaderPanel.less");
-    //console.log(style);
     const {headerItems, headerLogo} = this.props;
     return (
       <div >
