fa56ab37e6282bdeacea587baed54a2695d186b2
nguyenvantan <nguyenvantan@chotot.vn>
2016-03-25 13:55:03 +0700

    update footer

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 // export this package's api
 import HeaderPanel1 from './components/HeaderPanel/HeaderPanel';
 import FooterPanel1 from './components/FooterPanel/FooterPanel';
-export default HeaderPanel1;
+export  default FooterPanel1;
