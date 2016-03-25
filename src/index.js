5c118ce80787aee43967cf6caf1153b27a192181
nguyenvantan <nguyenvantan@chotot.vn>
2016-03-25 14:25:03 +0700

    update package

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 // export this package's api
-import HeaderPanel1 from './components/HeaderPanel/HeaderPanel';
-import FooterPanel1 from './components/FooterPanel/FooterPanel';
-export  {FooterPanel1, HeaderPanel1} ;
+import HeaderPanel from './components/HeaderPanel/HeaderPanel';
+import FooterPanel from './components/FooterPanel/FooterPanel';
+export  {FooterPanel, HeaderPanel} ;
